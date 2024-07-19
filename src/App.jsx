43b3d40550294b7301430fed191fe415f0703f2f
Gabriel Y Glezc C <gayagocr@gmail.com>
2024-07-19 00:39:21 -0500

    Feature(Sidebar para navegacion. Iconos y lista de opciones en webapp de rutas)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import './App.css'
+import Dashboard from './dashboard/Dashboard'
 
 function App () {
   return (
     <>
-      <div className='collapse bg-base-200'>
-        <input type='radio' name='my-accordion-1' defaultChecked />
-        <div className='collapse-title text-xl font-medium'>Click to open this one and close others</div>
-        <div className='collapse-content'>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className='collapse bg-base-200'>
-        <input type='radio' name='my-accordion-1' />
-        <div className='collapse-title text-xl font-medium'>Click to open this one and close others</div>
-        <div className='collapse-content'>
-          <p>hello</p>
-        </div>
-      </div>
-      <div className='collapse bg-base-200'>
-        <input type='radio' name='my-accordion-1' />
-        <div className='collapse-title text-xl font-medium'>Click to open this one and close others</div>
-        <div className='collapse-content'>
-          <p>hello</p>
-        </div>
-      </div>
+      <Dashboard />
     </>
   )
 }
