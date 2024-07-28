41e6b4591e6979b8abc5e7ae4be9fca16884d8bd
Gabriel Y Glezc C <gayagocr@gmail.com>
2024-07-28 00:34:08 -0600

    Fix(Correccion de texto al momento de mostrar error por login)

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
             <main
               className="h-[92vh] w-full flex flex-wrap items-center justify-center">
               <LoginForm />
-                { hasError && (<div role="alert" className="alert alert-error">
+                { hasError && (<div role="alert" className="max-w-[400px] alert alert-error">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6 shrink-0 stroke-current"
@@ -34,7 +34,7 @@ export default function LoginPage() {
                       strokeWidth="2"
                       d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                <span>Error! Task failed successfully.</span>
+                <span>Hubo un error</span>
               </div>)}
             </main>
         </div>
