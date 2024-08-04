5ff3bc2f7f75383c6b242184517ae7bb9f444dae
Gabriel Y Glezc C <gayagocr@gmail.com>
2024-08-04 13:44:15 -0600

    se añadio loader al registrarse y loguearse

--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux'
 import RegisterForm from '../../Layouts/RegisterLayout/RegisterForm/RegisterForm'
 import RegisterHeader from '../../Layouts/RegisterLayout/RegisterHeader/RegisterHeader'
+import useLoader from '../../customHooks/useLoader'
 
 export default function RegisterPage () {
+  const {isLoading} = useSelector(state => state.user)
+  const {loader} = useLoader(isLoading)
   return (
     <div className='bg-white h-screen w-full flex flex-col'>
       <header className='h-[8vh]'>
@@ -11,6 +15,7 @@ export default function RegisterPage () {
         className='h-[92vh] w-full flex flex-wrap items-center justify-center'
       >
         <RegisterForm />
+        {loader}
       </main>
     </div>
   )
